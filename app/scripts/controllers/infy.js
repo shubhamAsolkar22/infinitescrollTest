3193f8cae71b31e5582171df5f448ef708d5839e
shubhamAsolkar22 <38282971+shubhamAsolkar22@users.noreply.github.com>
2019-01-13 00:49:20 +0530

    getting infinite scroll effect without decoupling pagination

--- app/scripts/controllers/infy.js
+++ app/scripts/controllers/infy.js
@@ -7,56 +7,196 @@
  * # InfyctrlCtrl
  * Controller of the infyscrollApp
  */
-angular.module('infyscrollApp',['ui.grid', 'ui.grid.infiniteScroll'])
+angular.module('infyscrollApp',['ui.grid', 'ui.grid.pagination'])
   .controller('InfyctrlCtrl', function ($scope, $http, $timeout) {
-    var vm = this;
-    $scope.counter = 0;
-    vm.addData = function(){
-      $scope.Mdata.push({id:1000,name:'ssss',age:203});
+   
+   
+    $scope.addData = function() {
+      /* var n = $scope.gridOpts.data.length + 1;
+      debugger;
+      let a = [];
+      for(let i=1;i<5;i++){
+        a.push({
+          "firstName": "New " + n,
+          "lastName": "Person " + n,
+          "company": "abc",
+          "employed": true,
+          "gender": "male"
+        });
+        n++;
+      }
+      $scope.gridOpts.data = $scope.gridOpts.data.concat(a); */
+      $scope.gridOpts.paginationPageSize +=2;
     };
-    vm.gridOptions = {
-      
+   
+   
+   
+   
+   
+    var columnDefs1 = [
+      { name: 'firstName' },
+      { name: 'lastName' },
+      { name: 'company' },
+      { name: 'gender' }
+    ];
+   
+    var data1 = [
+      {
+        "firstName": "Cox",
+        "lastName": "Carney",
+        "company": "Enormo",
+        "gender": "male"
+      },
+      {
+        "firstName": "Lorraine",
+        "lastName": "Wise",
+        "company": "Comveyer",
+        "gender": "female"
+      },
+      {
+        "firstName": "Nancy",
+        "lastName": "Waters",
+        "company": "Fuelton",
+        "gender": "female"
+      },
+      {
+        "firstName": "Misty",
+        "lastName": "Oneill",
+        "company": "Letpro",
+        "gender": "female"
+      },{
+        "firstName": "Waters",
+        "lastName": "Shepherd",
+        "company": "Kongene",
+        "employed": true
+      },
+      {
+        "firstName": "Hopper",
+        "lastName": "Zamora",
+        "company": "Acium",
+        "employed": true
+      },
+      {
+        "firstName": "Marcy",
+        "lastName": "Mclean",
+        "company": "Zomboid",
+        "employed": true
+      },
+      {
+        "firstName": "Tania",
+        "lastName": "Cruz",
+        "company": "Marqet",
+        "employed": true
+      },
+      {
+        "firstName": "Kramer",
+        "lastName": "Cline",
+        "company": "Parleynet",
+        "employed": false
+      },
+      {
+        "firstName": "Bond",
+        "lastName": "Pickett",
+        "company": "Brainquil",
+        "employed": false
+      },{
+        "firstName": "Cox",
+        "lastName": "Carney",
+        "company": "Enormo",
+        "gender": "male"
+      },
+      {
+        "firstName": "Lorraine",
+        "lastName": "Wise",
+        "company": "Comveyer",
+        "gender": "female"
+      },
+      {
+        "firstName": "Nancy",
+        "lastName": "Waters",
+        "company": "Fuelton",
+        "gender": "female"
+      },
+      {
+        "firstName": "Misty",
+        "lastName": "Oneill",
+        "company": "Letpro",
+        "gender": "female"
+      },{
+        "firstName": "Waters",
+        "lastName": "Shepherd",
+        "company": "Kongene",
+        "employed": true
+      },
+      {
+        "firstName": "Hopper",
+        "lastName": "Zamora",
+        "company": "Acium",
+        "employed": true
+      },
+      {
+        "firstName": "Marcy",
+        "lastName": "Mclean",
+        "company": "Zomboid",
+        "employed": true
+      },
+      {
+        "firstName": "Tania",
+        "lastName": "Cruz",
+        "company": "Marqet",
+        "employed": true
+      },
+      {
+        "firstName": "Kramer",
+        "lastName": "Cline",
+        "company": "Parleynet",
+        "employed": false
+      },
+      {
+        "firstName": "Bond",
+        "lastName": "Pickett",
+        "company": "Brainquil",
+        "employed": false
+      }
+    ];
+   
+   
+    var columnDefs2 = [
+      { name: 'firstName' },
+      { name: 'lastName' },
+      { name: 'company' },
+      { name: 'employed' }
+    ];
+   
+    
+   
+   
+    $scope.gridOpts = {
+      columnDefs: columnDefs1,
+      data: data1,
+      paginationPageSize: 10,
       customScroller: function myScrolling(uiGridViewport, scrollHandler) {
         uiGridViewport.on('scroll', function myScrollingOverride(event) {
-  
+          $timeout(function(){
+
             let viewport = event.target;
             let canvas = viewport.querySelector('.ui-grid-canvas');
            
            if(viewport.offsetHeight + viewport.scrollTop > canvas.scrollHeight){
-            console.log('Yeah!!',$scope.counter++,$scope.Mdata);
-             $scope.Mdata.push({id:1000,name:'ssss',age:203});         
+            console.log('Yeah!!',$scope.data1);
+            /* $scope.gridOpts.data.push({
+              "firstName": "Cox",
+              "lastName": "Carney",
+              "company": "Enormo",
+              "gender": "male"
+            }); */
+            $scope.addData();                    
           }
-        })},
-
-      columnDefs: [
-        { name:'id'},
-        { name:'name' },
-        { name:'age' }
-      ],
-      data: 'Mdata',
-    
+        });
+        }
+        )}
+      
     };
-   
-   
-    $scope.Mdata = [{id:1,name:'sdssd sds',age:23}
-    ,{id:1,name:'sdssd sds',age:23}
-    ,{id:1,name:'sdssd sds',age:23}
-    ,{id:1,name:'sdssd sds',age:23}
-    ,{id:1,name:'sdssd sds',age:23}
-    ,{id:1,name:'sdssd sds',age:23}
-    ,{id:1,name:'sdssd sds',age:23}
-    ,{id:1,name:'sdssd sds',age:23}
-    ,{id:1,name:'sdssd sds',age:23}
-    ,{id:1,name:'sdssd sds',age:23}
-    ,{id:1,name:'sdssd sds',age:23}
-    ,{id:1,name:'sdssd sds',age:23}
-    ,{id:1,name:'sdssd sds',age:23}
-    ,{id:1,name:'sdssd sds',age:23}
-    ,{id:1,name:'sdssd sds',age:23}
-    ,{id:1,name:'sdssd sds',age:23}
-    ,{id:1,name:'sdssd sds',age:23}
-    ,{id:1,name:'sdssd sds',age:23}];
-   
   });
 
 
